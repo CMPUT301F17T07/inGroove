13242e78be0bdb7f27f841579490004a0ea89c59
AustinGrey <corey.crapo@gmail.com>
2017-10-21 15:35:20 -0600

    Added all the activities basic files

---
+++ inGroove/app/src/main/Java/com/cmput301f17t07/ingroove/CurrentHabitsActivity.java
@@ -0,0 +1,13 @@
+package com.cmput301f17t07.ingroove;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CurrentHabitsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_current_habits);
+    }
+}
