b6ae0b348b8b293b8c8ca54d5c6581d5259752dc
aholgate <aholgate@ualberta.ca>
2017-10-22 12:59:45 -0600

    outline of class and tests added

---
+++ inGroove/app/src/main/Java/com/cmput301f17t07/ingroove/RelationshipManager.java
@@ -0,0 +1,46 @@
+package com.cmput301f17t07.ingroove;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Ashley on 2017-10-22.
+ */
+
+public class RelationshipManager {
+
+    public RelationshipManager() {
+
+    }
+
+    public ArrayList<Integer> getFollowersof(User user) {
+
+    }
+
+    public ArrayList<Integer> getRequestsToFollow(User user) {
+
+    }
+
+    public ArrayList<Integer> getRequestsBy(User user) {
+
+    }
+
+    public ArrayList<Integer> getWhoFollows(User user) {
+
+    }
+
+    public Boolean isUserFollowedBy(User user1, User user2) {
+
+    }
+
+    public Boolean isUserFollowing(User user1, User user2) {
+
+    }
+
+    public void sendFollowRequest(User user1, User user2) {
+
+    }
+
+    public void acceptFollowRequest(User user1, User user2) {
+
+    }
+}
---
+++ inGroove/app/src/test/java/com/cmput301f17t07/ingroove/RelationshipManagerTest.java
@@ -0,0 +1,47 @@
+package com.cmput301f17t07.ingroove;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Ashley on 2017-10-22.
+ */
+
+public class RelationshipManagerTest {
+
+    public void testGetFollowersof() {
+        User user1 = new User("Test1", "test1@test.com", 1); //assuming constructor is User(name, email, id)
+        User user2 = new User("Test2", "test2@test.com", 2);
+        RelationshipManager relationships = new RelationshipManager();
+
+
+    }
+
+    public void testGetRequestsToFollow() {
+
+    }
+
+    public void testGetRequestsBy() {
+
+    }
+
+    public void testGetWhoFollows() {
+
+    }
+
+    public void testIsUserFollowedBy() {
+
+    }
+
+    public void testIsUserFollowing() {
+
+    }
+
+    public void testSendFollowRequest() {
+        
+    }
+
+    public void testAcceptFollowRequest() {
+
+    }
+
+}
