ae7fcd7339a63f50d21bade70ee839e88b931807
AustinGrey <corey.crapo@gmail.com>
2017-10-31 19:07:47 -0600

    Navigation drawer is working!

---
+++ inGroove/app/src/main/Java/com/cmput301f17t07/ingroove/Fragments/CurrentHabitsFragment.java
@@ -0,0 +1,32 @@
+package com.cmput301f17t07.ingroove.Fragments;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.cmput301f17t07.ingroove.R;
+
+public class CurrentHabitsFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        //returning our layout file
+        return inflater.inflate(R.layout.activity_current_habits, container, false);
+    }
+
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        //you can set the title for your toolbar here for different fragments different titles
+        getActivity().setTitle("Current Habits");
+    }
+}
