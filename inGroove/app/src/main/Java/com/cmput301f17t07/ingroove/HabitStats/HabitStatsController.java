8b39387fd26c06cce5d9bd8f2f0269f87b7c6d46
Adam Otto <aotto@cs.ualberta.ca>
2017-11-07 18:05:49 -0700

    Removed Java directory

--- inGroove/app/src/main/Java/com/cmput301f17t07/ingroove/HabitStats/HabitStatsController.java
+++
@@ -1,8 +0,0 @@
-package com.cmput301f17t07.ingroove.HabitStats;
-
-/**
- * Created by fraserbulbuc on 2017-10-24.
- */
-
-public class HabitStatsController {
-}
--- inGroove/app/src/main/Java/com/cmput301f17t07/ingroove/avehabit/AddViewEditHabitController.java
+++
@@ -1,9 +0,0 @@
-package com.cmput301f17t07.ingroove.avehabit;
-
-/**
- * Created by fraserbulbuc on 2017-10-24.
- */
-
-public class AddViewEditHabitController {
-
-}
