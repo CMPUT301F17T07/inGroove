203c826c2f1a7fc365411d805b1930d195d3ae50
Jolton <chrisrwalter@me.com>
2017-11-10 16:32:12 -0700

    something broke but now its fixed

--- inGroove/app/src/main/Java/com/cmput301f17t07/ingroove/CurrentHabitsActivity.java
+++ inGroove/app/src/main/Java/com/cmput301f17t07/ingroove/CurrentHabitsActivity.java
@@ -67,7 +67,7 @@ public class CurrentHabitsActivity extends NavigationDrawerActivity{
             @Override
             public void onItemClick(AdapterView<?> parent, View v, int position, long id) {
                 Intent upcomingIntent = new Intent(v.getContext(), AddViewEditHabitActivity.class);
-                upcomingIntent.putExtra(AddViewEditHabitActivity.habit_key, HabitHolder.get(position));
+                upcomingIntent.putExtra("ID", HabitHolder.get(position).getHabitID());
                 startActivityForResult(upcomingIntent, 0);
             }
         });
