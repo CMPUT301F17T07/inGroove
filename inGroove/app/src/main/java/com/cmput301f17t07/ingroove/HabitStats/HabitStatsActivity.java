13b5ae2c6b387a5c7970918b54cd3e4cd9b500e1
aholgate <aholgate@ualberta.ca>
2017-11-10 11:50:07 -0700

    more updates

--- inGroove/app/src/main/java/com/cmput301f17t07/ingroove/HabitStats/HabitStatsActivity.java
+++ inGroove/app/src/main/java/com/cmput301f17t07/ingroove/HabitStats/HabitStatsActivity.java
@@ -20,6 +20,11 @@ import java.util.Calendar;
 import java.util.Date;
 import java.util.GregorianCalendar;
 
+import static com.cmput301f17t07.ingroove.Model.Day.FRIDAY;
+import static com.cmput301f17t07.ingroove.Model.Day.SATURDAY;
+import static com.cmput301f17t07.ingroove.Model.Day.THURSDAY;
+import static com.cmput301f17t07.ingroove.Model.Day.TUESDAY;
+
 public class HabitStatsActivity extends AppCompatActivity {
 
     DataManagerAPI data = new MockDataManager().getInstance();
@@ -33,47 +38,36 @@ public class HabitStatsActivity extends AppCompatActivity {
 
     ArrayList<HabitEvent> habitEvents;
 
+    Habit passedHabit;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_habit_stats);
 
-        Habit passedHabit;
-
         Bundle bundle = this.getIntent().getExtras();
 
-
-        if (bundle != null){
-        //if (bundle == null) {
-
-            // this isn't working properly yet, but it will be
-            // but right noe the other section in else is running anyway, so it's okayish and doesn't break right away
-            
-            //passedHabit = (Habit) bundle.getSerializable("display_stats_for_habit");
+        if (bundle != null){      // use this for the real app
+        //if (bundle == null) {       // use this for testing with the mock data manager where things aren't getting passed
 
             // make work for mock data manager
             data.addUser("test");
             User user = data.getUser();
             ArrayList<Habit> habits = data.getHabit(user);
             passedHabit = habits.get(0);
+            ArrayList<Day> repeat = new ArrayList<Day>();
+            repeat.add(TUESDAY);
+            repeat.add(THURSDAY);
+            repeat.add(FRIDAY);
+            repeat.add(SATURDAY);
+            passedHabit.setRepeatedDays(repeat);
+            // end whats needed for mock data manager
 
-
-            // unbundle or something
             Log.w("TEST TEST TEST","passed bundle != null");
 
-            // fill in the habit data
-            // TextView habitTitle = (TextView) findViewById(R.id.habitStatsTitle);
-            // habitTitle.setText("super awesome habit to do");
-
+            // for the real data manager
+            //passedHabit = (Habit) bundle.getSerializable("display_stats_for_habit");
             habitEvents = data.getHabitEvents(passedHabit);
-
-            // first we get all the data we need
-            // habit doesn't have a start day, so we parse through habit events
-            // find the first start day, use as begining
-            // look at repeat cycle and see how many we habit events we should have between
-            // then and now
-            // difference is missed
-            // use this difference to get the progress bar amount
 
             // get the first day of the habit
             Date startDate = new Date();
@@ -82,30 +76,42 @@ public class HabitStatsActivity extends AppCompatActivity {
                     startDate = event.getDay();
                 }
             }
-            Log.w("TEST TEST TEST", "the min day is " + String.valueOf(startDate));
 
             // check to see how many habit events we should have
             int repeatedDays = passedHabit.getRepeatedDays().size(); // get number of days per week that we repeat
-            int weeks = 0;
+            int weeks = 1;
             Calendar cal = new GregorianCalendar();
             while (cal.getTime().before(new Date())) {
                 cal.add(Calendar.WEEK_OF_YEAR, 1);
                 weeks++;
             }
+
             int totalPossibleDays = repeatedDays * weeks;
             int completedDays = habitEvents.size();
+            float progress = ((completedDays / totalPossibleDays) * 100);
 
-            // then we set the data equal to what we need
+            Log.w("TEST TEST TEST", "the min day is " + String.valueOf(startDate));
+            Log.w("TEST TEST TEST", "total possible days " + String.valueOf(totalPossibleDays));
+            Log.w("TEST TEST TEST", "completed days " + String.valueOf(completedDays));
+            Log.w("TEST TEST TEST", "progress bar " + String.valueOf(progress));
+
+            // fill in the habit data
+            // TextView habitTitle = (TextView) findViewById(R.id.habitStatsTitle);
+            // habitTitle.setText("super awesome habit to do");
+
             // give completed habits the number of habit events
             completedHabits = (TextView) findViewById(R.id.completed_value);
             completedHabits.setText(String.valueOf(completedDays));
 
+            // give the missed habits the number of calculate missed habit events
             missedHabits = (TextView) findViewById(R.id.missed_value);
             missedHabits.setText(String.valueOf(totalPossibleDays - completedDays));
 
+            // give the progress the bar the calculated progress level
             habitProgress = (ProgressBar) findViewById(R.id.habitStatsProgressBar);
-            habitProgress.setProgress((completedDays / totalPossibleDays) * 100);
+            habitProgress.setProgress(Math.round(progress));
 
+            // view the needed calendar
             habitCalendar = (CalendarView) findViewById(R.id.habitStatsCalendarView);
             habitCalendar.setFirstDayOfWeek(1);
 
