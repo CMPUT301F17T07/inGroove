c9275aa28dc372730df431bc47a08f9b1e6d2cdd
AustinGrey <corey.crapo@gmail.com>
2017-11-11 12:46:54 -0700

    Splitting off of the Add Edit and View Habit views

---
+++ inGroove/app/src/main/java/com/cmput301f17t07/ingroove/avehabit/AddHabitActivity.java
@@ -0,0 +1,15 @@
+package com.cmput301f17t07.ingroove.avehabit;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.cmput301f17t07.ingroove.R;
+
+public class AddHabitActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_habit);
+    }
+}
