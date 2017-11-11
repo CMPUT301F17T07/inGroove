641591601efd80138ee859bd193f8d316cb2bc4a
fbulbuc <fbulbuc@ualberta.ca>
2017-11-11 11:38:49 -0700

    added method for converting location string to LatLng

--- inGroove/app/src/main/java/com/cmput301f17t07/ingroove/Model/HabitEvent.java
+++ inGroove/app/src/main/java/com/cmput301f17t07/ingroove/Model/HabitEvent.java
@@ -2,6 +2,8 @@ package com.cmput301f17t07.ingroove.Model;
 
 import android.graphics.Bitmap;
 
+import com.google.android.gms.maps.model.LatLng;
+
 import java.io.Serializable;
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -33,8 +35,14 @@ public class HabitEvent implements Serializable, Identifiable{
     public String getID() { return eventID; }
     public void setEventID(String id) {this.eventID = id;}
     //Location getter/setter
-    public String getLocation() { return location; }
-    public void setLocation(String location) {this.location = location;}
+    public String getLocation() {
+        if (location == null) {
+            return "";
+        } else {
+            return location;
+        }
+    }
+    public void setLocation(String location) {  this.location = location;}
 
     public String getHabitID() {
         return habitID;
@@ -55,12 +63,36 @@ public class HabitEvent implements Serializable, Identifiable{
         this.location = location;
     }
 
+    public HabitEvent(String name, String comment, Date day, Bitmap photo, String HabitID, String UserID){
+        this.name = name;
+        this.comment = comment;
+        this.day = day;
+        this.photo = photo;
+        this.habitID = HabitID;
+        this.userID = UserID;
+    }
+
+    public HabitEvent(String name, String comment, Date day, String HabitID, String UserID, String location){
+        this.name = name;
+        this.comment = comment;
+        this.day = day;
+        this.habitID = HabitID;
+        this.userID = UserID;
+        this.location = location;
+    }
+
     public HabitEvent(String name, String comment, Date day, String habitID, String userID) {
         this.name = name;
         this.comment = comment;
         this.day = day;
         this.habitID = habitID;
         this.userID = userID;
+    }
+
+    public HabitEvent(String name, Date day, String location) {
+        this.location = location;
+        this.name = name;
+        this.day = day;
     }
 
     public HabitEvent(String name, Date day) {
@@ -84,5 +116,19 @@ public class HabitEvent implements Serializable, Identifiable{
             return true;
         else
             return false;
+    }
+
+    public LatLng locationToLatLng() {
+
+        if (!location.equals("") || location != null) {
+            String[] latlng = location.split(",");
+            double lat = Double.parseDouble(latlng[0]);
+            double lng = Double.parseDouble(latlng[1]);
+            return new LatLng(lat, lng);
+        } else {
+            // Default location is UofA for now
+            // coordinates: 53.5232,-113.5263
+            return new LatLng(53.5232, -113.5263);
+        }
     }
 }
