9235d294db88b839c1e74fdf045bdbc7bb1687a4
aholgate <aholgate@ualberta.ca>
2017-11-13 10:37:00 -0700

    changed edit user view

--- inGroove/app/src/main/java/com/cmput301f17t07/ingroove/UserActivityPackage/EditUserActivity.java
+++ inGroove/app/src/main/java/com/cmput301f17t07/ingroove/UserActivityPackage/EditUserActivity.java
@@ -2,12 +2,15 @@ package com.cmput301f17t07.ingroove.UserActivityPackage;
 
 import android.content.Context;
 import android.content.Intent;
+import android.media.Image;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 
 import com.cmput301f17t07.ingroove.DataManagers.Command.DataManagerAPI;
 import com.cmput301f17t07.ingroove.DataManagers.DataManager;
@@ -37,6 +40,9 @@ public class EditUserActivity extends AppCompatActivity {
     EditText userName;
     EditText userEmail;
     Button saveUser;
+    Button goBack;
+    ImageButton editUserImage;
+    ImageView userImage;
 
     /**
      *
@@ -54,13 +60,18 @@ public class EditUserActivity extends AppCompatActivity {
             userName = (EditText) findViewById(R.id.userName);
             userEmail = (EditText) findViewById(R.id.userEmail);
             saveUser = (Button) findViewById(R.id.saveUser);
+            goBack = (Button) findViewById(R.id.editUserBackButton);
+            editUserImage = (ImageButton) findViewById(R.id.editImageButton);
+            userImage = (ImageView) findViewById(R.id.userImageView);
 
             // fill them in with the current info for user
             userName.setText(user.getName());
             userEmail.setText(user.getEmail());
 
-            // if save button clicked then save and return back to UserActivity
+
             final Context context = this.getApplicationContext();
+
+            // if save button clicked then save and return back to UserActivity
             saveUser.setOnClickListener(new View.OnClickListener() {
                 public void onClick(View v) {
                     // grab the text view elements
@@ -77,6 +88,25 @@ public class EditUserActivity extends AppCompatActivity {
                     returnIntent.putExtra(UserActivity.return_user_key, user);
                     setResult(RESULT_OK, returnIntent);
                     finish();
+                }
+            });
+
+            // back button is pressed, do not save changes
+            goBack.setOnClickListener(new View.OnClickListener() {
+                public void onClick(View v) {
+                    // don't need to save anything, just return user as is
+
+                    // return back to UserActivity
+                    Intent returnIntent = new Intent(context, UserActivity.class);
+                    setResult(RESULT_CANCELED, returnIntent);
+                    finish();
+                }
+            });
+
+            // change the image for the user
+            editUserImage.setOnClickListener(new View.OnClickListener() {
+                public void onClick(View v) {
+
                 }
             });
 
