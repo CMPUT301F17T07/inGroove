10745ca213dc4cc6eb009e627b9e10d1a7b13c95
aholgate <aholgate@ualberta.ca>
2017-11-25 13:01:57 -0700

    updates to the follow activity

--- inGroove/app/src/main/java/com/cmput301f17t07/ingroove/FollowOtherUsers/FollowActivity.java
+++ inGroove/app/src/main/java/com/cmput301f17t07/ingroove/FollowOtherUsers/FollowActivity.java
@@ -5,8 +5,11 @@ import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.ListView;
 
+import com.cmput301f17t07.ingroove.Model.Follow;
 import com.cmput301f17t07.ingroove.Model.User;
 import com.cmput301f17t07.ingroove.R;
 import com.cmput301f17t07.ingroove.navDrawer.NavigationDrawerActivity;
@@ -19,9 +22,17 @@ import java.util.ArrayList;
  * @see User
  */
 public class FollowActivity extends NavigationDrawerActivity {
-    private FollowAdapter followAdapter;
-    private ArrayList<User> searchedForUserList = new ArrayList<User>();
-    private ListView searchedForUserListView;
+
+    // elements on the view
+    ListView otherUsersList;
+    EditText searchBox;
+    ImageButton searchButton;
+
+    // adapter
+    FollowAdapter followAdapter;
+    ArrayList<User> searchedForUserList = new ArrayList<User>();
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,11 +40,19 @@ public class FollowActivity extends NavigationDrawerActivity {
         setContentView(R.layout.activity_follow);
         super.onCreateDrawer();
 
+        User user1 = new User("Bob");
+        User user2 = new User("Joe");
+        //Follow follow1 = new Follow()
+        searchedForUserList.add(user1);
+        searchedForUserList.add(user2);
 
+        // find all the elements on the page
+        searchBox.findViewById(R.id.searchForUsersButton);
+        searchButton.findViewById(R.id.searchForUsersButton);
         // get and set adapters for the list view
-        searchedForUserListView.findViewById(R.id.followUsersListView);
+        otherUsersList.findViewById(R.id.followUsersListView);
         followAdapter = new FollowAdapter(this, R.layout.list_item_follow_activity, searchedForUserList);
-        searchedForUserListView.setAdapter(followAdapter);
+        otherUsersList.setAdapter(followAdapter);
     }
 
 
