ea99c37c17be3d85620bc8f44fad80bf2f932869
AustinGrey <corey.crapo@gmail.com>
2017-11-28 18:20:38 -0700

    Attempting location real time fixes

--- inGroove/app/src/main/Java/com/cmput301f17t07/ingroove/MapsActivity.java
+++ inGroove/app/src/main/Java/com/cmput301f17t07/ingroove/MapsActivity.java
@@ -2,9 +2,11 @@ package com.cmput301f17t07.ingroove;
 
 import android.Manifest;
 import android.content.Intent;
+import android.content.IntentSender;
 import android.content.pm.PackageManager;
 import android.location.Location;
 import android.net.Uri;
+import android.os.Looper;
 import android.provider.Settings;
 import android.support.annotation.NonNull;
 import android.support.design.widget.Snackbar;
@@ -13,13 +15,24 @@ import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Toast;
 
 import com.cmput301f17t07.ingroove.DataManagers.Command.DataManagerAPI;
 import com.cmput301f17t07.ingroove.DataManagers.DataManager;
 import com.cmput301f17t07.ingroove.Model.Habit;
 import com.cmput301f17t07.ingroove.Model.HabitEvent;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.ResolvableApiException;
 import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
 import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.LocationSettingsRequest;
+import com.google.android.gms.location.LocationSettingsResponse;
+import com.google.android.gms.location.LocationSettingsStatusCodes;
+import com.google.android.gms.location.SettingsClient;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -27,9 +40,13 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 
+import java.text.DateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.Random;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
@@ -63,6 +80,20 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+        /* Imported */
+        mRequestingLocationUpdates = false;
+        mLastUpdateTime = "";
+
+        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+        mSettingsClient = LocationServices.getSettingsClient(this);
+
+        // Kick off the process of building the LocationCallback, LocationRequest, and
+        // LocationSettingsRequest objects.
+        createLocationCallback();
+        createLocationRequest();
+        buildLocationSettingsRequest();
+        /* END IMPORTED */
 
         // Location setup
         mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
@@ -282,4 +313,242 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
             }
         }
     }
+
+
+    /**
+     * Constant used in the location settings dialog.
+     */
+    private static final int REQUEST_CHECK_SETTINGS = 0x1;
+
+        /**
+         * The desired interval for location updates. Inexact. Updates may be more or less frequent.
+         */
+        private static final long UPDATE_INTERVAL_IN_MILLISECONDS = 10000;
+
+        /**
+         * The fastest rate for active location updates. Exact. Updates will never be more frequent
+         * than this value.
+         */
+        private static final long FASTEST_UPDATE_INTERVAL_IN_MILLISECONDS =
+                UPDATE_INTERVAL_IN_MILLISECONDS / 2;
+
+        // Keys for storing activity state in the Bundle.
+        private final static String KEY_REQUESTING_LOCATION_UPDATES = "requesting-location-updates";
+        private final static String KEY_LOCATION = "location";
+        private final static String KEY_LAST_UPDATED_TIME_STRING = "last-updated-time-string";
+
+        /**
+         * Provides access to the Location Settings API.
+         */
+        private SettingsClient mSettingsClient;
+
+        /**
+         * Stores parameters for requests to the FusedLocationProviderApi.
+         */
+        private LocationRequest mLocationRequest;
+
+        /**
+         * Stores the types of location services the client is interested in using. Used for checking
+         * settings to determine if the device has optimal location settings.
+         */
+        private LocationSettingsRequest mLocationSettingsRequest;
+
+        /**
+         * Callback for Location events.
+         */
+        private LocationCallback mLocationCallback;
+
+        /**
+         * Represents a geographical location.
+         */
+        private Location mCurrentLocation;
+
+        /**
+         * Tracks the status of the location updates request. Value changes when the user presses the
+         * Start Updates and Stop Updates buttons.
+         */
+        private Boolean mRequestingLocationUpdates;
+
+        /**
+         * Time when the location was updated represented as a String.
+         */
+        private String mLastUpdateTime;
+
+        /**
+         * Sets up the location request. Android has two location request settings:
+         * {@code ACCESS_COARSE_LOCATION} and {@code ACCESS_FINE_LOCATION}. These settings control
+         * the accuracy of the current location. This sample uses ACCESS_FINE_LOCATION, as defined in
+         * the AndroidManifest.xml.
+         * <p/>
+         * When the ACCESS_FINE_LOCATION setting is specified, combined with a fast update
+         * interval (5 seconds), the Fused Location Provider API returns location updates that are
+         * accurate to within a few feet.
+         * <p/>
+         * These settings are appropriate for mapping applications that show real-time location
+         * updates.
+         */
+        private void createLocationRequest() {
+            mLocationRequest = new LocationRequest();
+
+            // Sets the desired interval for active location updates. This interval is
+            // inexact. You may not receive updates at all if no location sources are available, or
+            // you may receive them slower than requested. You may also receive updates faster than
+            // requested if other applications are requesting location at a faster interval.
+            mLocationRequest.setInterval(UPDATE_INTERVAL_IN_MILLISECONDS);
+
+            // Sets the fastest rate for active location updates. This interval is exact, and your
+            // application will never receive updates faster than this value.
+            mLocationRequest.setFastestInterval(FASTEST_UPDATE_INTERVAL_IN_MILLISECONDS);
+
+            mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        }
+
+        /**
+         * Creates a callback for receiving location events.
+         */
+        private void createLocationCallback() {
+            mLocationCallback = new LocationCallback() {
+                @Override
+                public void onLocationResult(LocationResult locationResult) {
+                    super.onLocationResult(locationResult);
+
+                    mCurrentLocation = locationResult.getLastLocation();
+                    mLastUpdateTime = DateFormat.getTimeInstance().format(new Date());
+                }
+            };
+        }
+
+        /**
+         * Uses a {@link com.google.android.gms.location.LocationSettingsRequest.Builder} to build
+         * a {@link com.google.android.gms.location.LocationSettingsRequest} that is used for checking
+         * if a device has the needed location settings.
+         */
+        private void buildLocationSettingsRequest() {
+            LocationSettingsRequest.Builder builder = new LocationSettingsRequest.Builder();
+            builder.addLocationRequest(mLocationRequest);
+            mLocationSettingsRequest = builder.build();
+        }
+
+        /**
+         * Handles the Start Updates button and requests start of location updates. Does nothing if
+         * updates have already been requested.
+         */
+        public void startUpdatesButtonHandler(View view) {
+            if (!mRequestingLocationUpdates) {
+                mRequestingLocationUpdates = true;
+                startLocationUpdates();
+            }
+        }
+
+        /**
+         * Handles the Stop Updates button, and requests removal of location updates.
+         */
+        public void stopUpdatesButtonHandler(View view) {
+            // It is a good practice to remove location requests when the activity is in a paused or
+            // stopped state. Doing so helps battery performance and is especially
+            // recommended in applications that request frequent location updates.
+            stopLocationUpdates();
+        }
+
+        /**
+         * Requests location updates from the FusedLocationApi. Note: we don't call this unless location
+         * runtime permission has been granted.
+         */
+        @SuppressWarnings("MissingPermission")
+        private void startLocationUpdates() {
+            // Begin by checking if the device has the necessary location settings.
+            mSettingsClient.checkLocationSettings(mLocationSettingsRequest)
+                    .addOnSuccessListener(this, new OnSuccessListener<LocationSettingsResponse>() {
+                        @Override
+                        public void onSuccess(LocationSettingsResponse locationSettingsResponse) {
+                            Log.i(TAG, "All location settings are satisfied.");
+
+                            mFusedLocationClient.requestLocationUpdates(mLocationRequest,
+                                    mLocationCallback, Looper.myLooper());
+
+                        }
+                    })
+                    .addOnFailureListener(this, new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            int statusCode = ((ApiException) e).getStatusCode();
+                            switch (statusCode) {
+                                case LocationSettingsStatusCodes.RESOLUTION_REQUIRED:
+                                    Log.i(TAG, "Location settings are not satisfied. Attempting to upgrade " +
+                                            "location settings ");
+                                    try {
+                                        // Show the dialog by calling startResolutionForResult(), and check the
+                                        // result in onActivityResult().
+                                        ResolvableApiException rae = (ResolvableApiException) e;
+                                        rae.startResolutionForResult(MapsActivity.this, REQUEST_CHECK_SETTINGS);
+                                    } catch (IntentSender.SendIntentException sie) {
+                                        Log.i(TAG, "PendingIntent unable to execute request.");
+                                    }
+                                    break;
+                                case LocationSettingsStatusCodes.SETTINGS_CHANGE_UNAVAILABLE:
+                                    String errorMessage = "Location settings are inadequate, and cannot be " +
+                                            "fixed here. Fix in Settings.";
+                                    Log.e(TAG, errorMessage);
+                                    Toast.makeText(MapsActivity.this, errorMessage, Toast.LENGTH_LONG).show();
+                                    mRequestingLocationUpdates = false;
+                            }
+                        }
+                    });
+        }
+
+
+
+        /**
+         * Removes location updates from the FusedLocationApi.
+         */
+        private void stopLocationUpdates() {
+            if (!mRequestingLocationUpdates) {
+                Log.d(TAG, "stopLocationUpdates: updates never requested, no-op.");
+                return;
+            }
+
+            // It is a good practice to remove location requests when the activity is in a paused or
+            // stopped state. Doing so helps battery performance and is especially
+            // recommended in applications that request frequent location updates.
+            mFusedLocationClient.removeLocationUpdates(mLocationCallback)
+                    .addOnCompleteListener(this, new OnCompleteListener<Void>() {
+                        @Override
+                        public void onComplete(@NonNull Task<Void> task) {
+                            mRequestingLocationUpdates = false;
+                        }
+                    });
+        }
+
+        @Override
+        public void onResume() {
+            super.onResume();
+            // Within {@code onPause()}, we remove location updates. Here, we resume receiving
+            // location updates if the user has requested them.
+            if (mRequestingLocationUpdates && checkPermissions()) {
+                startLocationUpdates();
+            } else if (!checkPermissions()) {
+                requestPermissions();
+            }
+        }
+
+        @Override
+        protected void onPause() {
+            super.onPause();
+
+            // Remove location updates to save battery.
+            stopLocationUpdates();
+        }
+
+        /**
+         * Stores activity data in the Bundle.
+         */
+        public void onSaveInstanceState(Bundle savedInstanceState) {
+            savedInstanceState.putBoolean(KEY_REQUESTING_LOCATION_UPDATES, mRequestingLocationUpdates);
+            savedInstanceState.putParcelable(KEY_LOCATION, mCurrentLocation);
+            savedInstanceState.putString(KEY_LAST_UPDATED_TIME_STRING, mLastUpdateTime);
+            super.onSaveInstanceState(savedInstanceState);
+        }
+
+
 }
+
