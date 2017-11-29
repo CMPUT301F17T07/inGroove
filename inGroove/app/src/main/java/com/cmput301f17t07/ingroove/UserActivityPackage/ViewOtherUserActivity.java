fd8f215f101bbdab6116ed3835072364f13c972a
Adam Otto <aotto@cs.ualberta.ca>
2017-11-28 18:06:40 -0700

    ListView now point to ViewOtherUserActivity

---
+++ inGroove/app/src/main/java/com/cmput301f17t07/ingroove/UserActivityPackage/ViewOtherUserActivity.java
@@ -0,0 +1,13 @@
+package com.cmput301f17t07.ingroove.UserActivityPackage;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ViewOtherUserActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_other_user);
+    }
+}
