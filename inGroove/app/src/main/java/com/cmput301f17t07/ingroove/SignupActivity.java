f86e546f8aab019134b6ede8f4abb595f5421100
AustinGrey <corey.crapo@gmail.com>
2017-11-28 19:34:22 -0700

    Fixed the signup activity

--- inGroove/app/src/main/java/com/cmput301f17t07/ingroove/SignupActivity.java
+++ inGroove/app/src/main/java/com/cmput301f17t07/ingroove/SignupActivity.java
@@ -1,18 +1,46 @@
 package com.cmput301f17t07.ingroove;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+
+import com.cmput301f17t07.ingroove.DataManagers.Command.DataManagerAPI;
+import com.cmput301f17t07.ingroove.DataManagers.DataManager;
+import com.cmput301f17t07.ingroove.Model.User;
 
 public class SignupActivity extends AppCompatActivity {
 
     Button save_btn;
+    EditText name;
+    EditText email;
+
+    User user;
+    DataManagerAPI data = DataManager.getInstance();
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+        user = data.getUser();
 
+        save_btn = (Button) findViewById(R.id.signup_saveUser);
+        name = (EditText) findViewById(R.id.signup_userName);
+        email = (EditText) findViewById(R.id.signup_userEmail);
+
+        save_btn.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                user.setName(name.getText().toString());
+                user.setEmail(email.getText().toString());
+                data.editUser(user);
+
+                Intent intent = new Intent(getApplicationContext(), CurrentHabitsActivity.class);
+                getApplicationContext().startActivity(intent);
+                finish();
+            }
+        });
     }
 }
