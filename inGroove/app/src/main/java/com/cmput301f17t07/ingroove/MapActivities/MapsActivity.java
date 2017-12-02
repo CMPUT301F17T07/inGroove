69ef026bc765b24f52c670aa02f5c2af3d28792c
AustinGrey <corey.crapo@gmail.com>
2017-12-02 16:35:12 -0700

    Real time location in habit event logging

--- inGroove/app/src/main/java/com/cmput301f17t07/ingroove/MapActivities/MapsActivity.java
+++ inGroove/app/src/main/java/com/cmput301f17t07/ingroove/MapActivities/MapsActivity.java
@@ -100,6 +100,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         // 5km of the user from the user him/her self.
         ArrayList<Habit> habits = data.getHabits();
 
+        Log.d("--MAP--", "Test");
         // Get all the users this user follows
         data.getWhoThisUserFollows(data.getUser(), new AsyncResultHandler<User>() {
             @Override
